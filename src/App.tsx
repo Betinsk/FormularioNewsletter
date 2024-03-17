75bbf9275671a7926a2105de73c70e99cdeda69d
betinsk <gmdust@hotmail.com>
2024-03-17 20:26:24 -0300

    inicio

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Form from './components/form'
+
+function App() {
+
+  return (
+    <>
+      <h1>Inscreva-se</h1>
+      <Form />
+    </>
+  )
+}
+
+export default App
